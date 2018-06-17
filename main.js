96f5b71cb43b9fd249ac8e5853e57445ef250e61
Michael Salvatore <msalvatore22@gmail.com>
2018-06-16 23:39:25 -0400

    website aligned better, added smooth scroll to projects btn

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".projects-btn").addEventListener('click', (event) => {
+	let section = document.getElementById("projects")
+	section.scrollIntoView({behavior: "smooth"});
+});
