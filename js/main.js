b05a92217c37eeaa4475a955b819200eeb8ba8c9
Michael Salvatore <msalvatore22@gmail.com>
2018-08-23 13:53:40 -0400

    got particles JS Working

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ document.querySelector(".projects-btn").addEventListener('click', (event) => {
 	let section = document.getElementById("projects")
 	section.scrollIntoView({behavior: "smooth"});
 });
+
+
