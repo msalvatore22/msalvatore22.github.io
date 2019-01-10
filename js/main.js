a9fd550733c9b7bc1e6a68ab5f9b0530ba4d29ec
Michael Salvatore <msalvatore22@gmail.com>
2019-01-10 17:25:51 -0500

    created the modal and modal open and close functionality

--- js/main.js
+++ js/main.js
@@ -16,3 +16,29 @@ document.getElementById("view-work").addEventListener('click', (event) => {
 	let section = document.getElementById("portfolio")
 	section.scrollIntoView({behavior: "smooth"});
 });
+
+// handle modal open and close
+
+let btns = document.querySelectorAll('.modal-btn')
+
+btns.forEach((btn) => {
+    btn.onclick = () => {
+        let modalId = btn.getAttribute('data-modal')
+        document.getElementById(modalId).style.display = 'block'
+    }
+})
+
+let closeBtns = document.querySelectorAll('.close')
+
+closeBtns.forEach((btn) => {
+    btn.onclick = () => {
+        let modal = btn.closest('.modal-container')
+        modal.style.display = 'none'
+    }
+})
+
+window.onclick = (event) => {
+    if(event.target.className === 'modal-container'){
+        event.target.style.display = 'none'
+    }
+}
