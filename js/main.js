1a7b6b09efa59d24fe3d178b4d8d64f5c037f151
Michael Salvatore <msalvatore22@gmail.com>
2019-01-22 22:32:45 -0500

    added hambuger menu open and close animation, added smooth scroll to section from hamburger menu, close menu on scroll to secction

--- js/main.js
+++ js/main.js
@@ -15,18 +15,24 @@ $(document).ready(function(){
 		},5000);
     });
     
-    // hambuger menu
+    // hambuger menu open and close
     $('#hamburger-menu-wrapper').click(()=>{
         $('.hamburger').toggleClass('close');
         $('#hamburger-menu').toggleClass('open');
     });
 
+    // close menu on nav click
+    $('.hamburger-nav').click(()=> {
+        $('.hamburger').toggleClass('close');
+        $('#hamburger-menu').toggleClass('open');
+    })
+
 })
 
-// scroll to projects section
+// scroll to view work
 
 document.getElementById("view-work").addEventListener('click', (event) => {
-	let section = document.getElementById("portfolio")
+	let section = document.getElementById("portfolio");
 	section.scrollIntoView({behavior: "smooth"});
 });
 
@@ -37,14 +43,27 @@ document.getElementById("to-top").addEventListener('click', (event) => {
 	section.scrollIntoView({behavior: "smooth"});
 });
 
+// scroll to sections from hamburger menu
+
+let navBtns = document.querySelectorAll('.hamburger-nav');
+
+navBtns.forEach((btn) => {
+    btn.onclick = () => {
+        let sectionId = btn.getAttribute('data-nav');
+        let section = document.getElementById(sectionId);
+        section.scrollIntoView({behavior: 'smooth'});
+    }
+})
+
+
 // handle modal open and close
 
-let btns = document.querySelectorAll('.modal-btn')
+let btns = document.querySelectorAll('.modal-btn');
 
 btns.forEach((btn) => {
     btn.onclick = () => {
-        let modalId = btn.getAttribute('data-modal')
-        document.getElementById(modalId).style.display = 'block'
+        let modalId = btn.getAttribute('data-modal');
+        document.getElementById(modalId).style.display = 'block';
     }
 })
 
@@ -53,13 +72,13 @@ let closeBtns = document.querySelectorAll('.close')
 closeBtns.forEach((btn) => {
     btn.onclick = () => {
         let modal = btn.closest('.modal-container')
-        modal.style.display = 'none'
+        modal.style.display = 'none';
     }
 })
 
 window.onclick = (event) => {
     if(event.target.className === 'modal-container'){
-        event.target.style.display = 'none'
+        event.target.style.display = 'none';
     }
 }
 
